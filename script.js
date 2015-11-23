204c98063650fa753277ab8ae20fdf4c78322d53
Vanya Kapitonov <moving.alpha@gmail.com>
2015-11-24 00:34:37 +1100

    mozhno pokazyvat'

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+//test    $('div').css('border', '3px solid red');
+});
+/*      $('td').hover(function() {
+	/*$(this).addClass('highlight');
+	ja poka ne soobrazhu, kak posvetit' ego -- pustj polzaet*/
+//	  $(this).animate({top: "+=15px", height: "toggle"}, 450, function() {
+/*	  $(this).addClass('red')}, function() {
+	      $(this).toggleClass('red');
+	  });
+/*    opacity: 0.25,
+    left: "+=50",
+    height: "toggle"
+  }, 5000, function() {
+    // Animation complete.
+  });*/
+/*{{top: '+=5px'}, 450}, {{top: '-=5px'}, 450});*/
+//});

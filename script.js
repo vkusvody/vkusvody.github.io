fa6e0b94c6b665d279ba157a28e450edae42bf75
Vanya Kapitonov <moving.alpha@gmail.com>
2020-12-20 00:05:28 +0100

    Learning document methods to be able to do nice mouseover events

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-/*$(document).ready(function() {
+$(document).ready(function() {
     $('div').css('border', '3px solid red');
-});*/
+});
+$(document).GetElementById("secNav").addEventListener("mouseover", function( event ) {
+	event.target.style.color = "red";}, true);
+});
 /*      $('td').hover(function() {
 	/*$(this).addClass('highlight');
 	ja poka ne soobrazhu, kak posvetit' ego -- pustj polzaet*/
-//	  $(this).animate({top: "+=15px", height: "toggle"}, 450, function() {
-/*	  $(this).addClass('red')}, function() {
+/*	  $(this).animate({top: "+=15px", height: "toggle"}, 450, function() {
+	  $(this).addClass('red')}, function() {
 	      $(this).toggleClass('red');
 	  });
-/*    opacity: 0.25,
+    opacity: 0.25,
     left: "+=50",
     height: "toggle"
   }, 5000, function() {
@@ -16,3 +19,7 @@
   });*/
 /*{{top: '+=5px'}, 450}, {{top: '-=5px'}, 450});*/
 //});
+
+let bulletPoint = document.GetElementById("secNav");
+bulletPoint.addEventListener("mouseover", function( event ) {
+    event.target.style.color = "orange";}, false);
